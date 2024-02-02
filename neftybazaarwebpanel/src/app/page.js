b66c631892954e6e00c92d53a223560667806993
Rezowan Sifat <navidrezowansifat@gmail.com>
2024-02-02 20:28:27 +0600

    getTopCreators bug fix

--- neftybazaarwebpanel/src/app/page.js
+++ neftybazaarwebpanel/src/app/page.js
@@ -3,16 +3,32 @@
 import FeaturedItem from "@/components/FeaturedItems/FeaturedItem";
 import { HeroSection } from "@/components/componentsindex";
 import Link from "next/link";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import NeftyBazaarContext from "../../Context/NeftyBazaarContext";
 import Loader from "@/components/Loader/Loader";
+import { getTopCreators } from "../../topCreators/topCreators";
+
 const Home = () => {
-  const { checkIFWalletConnected, currentAccount } =
+  const { fetchNFTs, checkIFWalletConnected, currentAccount } =
     useContext(NeftyBazaarContext);
+
+  const [nfts, setNfts] = useState([]);
+  // const [nftsCopy, setNftsCopy] = useState([]);
+
+  //Top Creator List
+  const TopCreators = getTopCreators(nfts);
+
+  console.log(TopCreators);
 
   useEffect(() => {
     checkIFWalletConnected();
+  }, []);
+
+  useEffect(() => {
+    fetchNFTs().then((items) => {
+      setNfts(items.reverse());
+    });
   }, []);
 
   return (
--- neftybazaarwebpanel/topCreators/topCreators.js
+++ neftybazaarwebpanel/topCreators/topCreators.js
@@ -2,14 +2,16 @@ export const getTopCreators = (creators) => {
   const finalCreators = [];
 
   const finalResults = creators.reduce((index, currentValue) => {
-    (index[currentValue.seller] || []).push(currentValue);
+    (index[currentValue.seller] = index[currentValue.seller] || []).push(
+      currentValue
+    );
 
     return index;
   }, {});
 
   Object.entries(finalResults).forEach((item) => {
     const seller = item[0];
-    const total = item[0]
+    const total = item[1]
       .map((newItem) => Number(newItem.price))
       .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
 
